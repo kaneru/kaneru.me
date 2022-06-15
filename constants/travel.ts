672982740ae4358904c2f38ca92462866fe2b0f8
kaneru <kaneru@fastmail.com>
2022-06-15 12:48:35 +0400

    feature: add constants

---
+++ constants/travel.ts
@@ -0,0 +1,32 @@
+type Trip = {
+  place: string;
+  date: string;
+  linkToPost?: string;
+};
+
+type TripsByYear = {
+  [key: string]: Trip[];
+};
+
+export const TRIPS_BY_YEAR: TripsByYear = {
+  2022: [
+    { place: 'Бишкек, Кыргызстан', date: 'Июнь' },
+    { place: 'Озеро Колсай, Казахстан', date: 'Апрель' },
+    { place: 'Алматы, Казахстан', date: 'Март' },
+    { place: 'Нур-Султан, Казахстан', date: 'Март' },
+    { place: 'Омск, Россия', date: 'Март' },
+    { place: 'Москва, Россия', date: 'Март' },
+    { place: 'Москва, Россия', date: 'Январь' },
+  ],
+  2021: [
+    { place: 'Москва, Россия', date: 'Декабрь' },
+    { place: 'Владикавказ, Россия', date: 'Декабрь' },
+    { place: 'Гора Казбек, Грузия ', date: 'Декабрь' },
+    { place: 'Тбилиси, Грузия', date: 'Декабрь' },
+    { place: 'Москва, Россия', date: 'Декабрь' },
+  ],
+  2016: [
+    { place: 'Пусан, Южная Корея', date: 'Август' },
+    { place: 'Сеул, Южная Корея', date: 'Июль — Август' },
+  ],
+};
