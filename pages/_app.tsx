aecd48c72fc640c5d219dc19df223cc09171d23f
kaneru <kaneru@fastmail.com>
2022-07-01 15:20:03 +0400

    feat: add canonical url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const canonicalURL = `https://kaneru.me${useRouter().asPath}`;
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalURL} />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
