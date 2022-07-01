6624d1e4199d9d216d913158f90a3bae2d97838e
kaneru <kaneru@fastmail.com>
2022-07-01 16:07:28 +0400

    fix: enable goatcounter only in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,21 @@ import { Footer } from '../components/footer';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 function MyApp({ Component, pageProps }: AppProps) {
   const canonicalURL = `https://kaneru.me${useRouter().asPath}`;
   return (
     <>
       <Head>
         <link rel="canonical" href={canonicalURL} />
-        <script
-          data-goatcounter="https://kaneru.goatcounter.com/count"
-          async
-          src="//gc.zgo.at/count.js"
-        />
+        {isProd && (
+          <script
+            data-goatcounter="https://kaneru.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+          />
+        )}
       </Head>
       <Header />
       <Component {...pageProps} />
