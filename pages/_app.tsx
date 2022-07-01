42b88f5c798432cc09d7790dde91c1d18a82ea95
kaneru <kaneru@fastmail.com>
2022-07-01 20:40:53 +0400

    feat: add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="canonical" href={canonicalURL} />
         {isProd && (
           <script
