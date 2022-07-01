0b98a62bc4a21d114fa7841e035e76e4d9e5a7df
kaneru <kaneru@fastmail.com>
2022-07-01 12:21:00 +0400

    feat: add mobile menu

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import cn from 'classnames';
 import { Container } from './container';
+import { useState } from 'react';
 
 const HEADER_MENU_ITEMS = [
   { id: 0, title: 'Посты', url: '/posts' },
@@ -14,6 +15,7 @@ const HEADER_MENU_ITEMS = [
 ];
 
 export const Header = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
   const router = useRouter();
   const isMainPage = router.pathname === '/';
 
@@ -27,10 +29,13 @@ export const Header = () => {
           {
             'bg-zinc-800 mb-10': !isMainPage,
           },
+          {
+            ['bg-zinc-800 lg:bg-transparent']: isMenuOpened,
+          },
         )}
       >
         <Container className="py-8">
-          <nav className="flex gap-8 items-end">
+          <nav className="flex flex-wrap gap-8 items-center lg:items-end justify-between lg:justify-start">
             <Link href="/">
               <a
                 className={cn('text-zinc-50 hover:text-blue-500', {
@@ -42,24 +47,64 @@ export const Header = () => {
                 </h1>
               </a>
             </Link>
-            <ul className="hidden gap-6 lg:flex">
-              {HEADER_MENU_ITEMS.map(item => (
-                <li key={item.id} className="text-lg font-medium">
-                  <Link href={item.url}>
-                    <a
-                      className={cn(
-                        'text-zinc-50 hover:underline hover:text-blue-500',
-                        {
-                          'hover:text-zinc-200': isMainPage,
-                        },
-                      )}
-                    >
-                      {item.title}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <button
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-zinc-50 rounded-lg lg:hidden hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setIsMenuOpened(!isMenuOpened)}
+            >
+              <span className="sr-only">Открыть навигационное меню</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              className={cn('w-full lg:block lg:w-auto', {
+                hidden: !isMenuOpened,
+              })}
+              id="mobile-menu"
+            >
+              <ul className="flex flex-col gap-6 lg:flex-row">
+                {HEADER_MENU_ITEMS.map(item => (
+                  <li key={item.id} className="text-lg font-medium">
+                    <Link href={item.url}>
+                      <a
+                        className={cn(
+                          'text-zinc-50 hover:underline hover:text-blue-500',
+                          {
+                            'hover:text-zinc-200': isMainPage,
+                          },
+                        )}
+                      >
+                        {item.title}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </nav>
         </Container>
       </header>
